e67bb8033d81a030e187081e2f366a036a7d2db1
yung-gimp <>
2024-12-21 16:07:54 -0500

    cleanup and remove unused hosts

--- flake.nix
+++ flake.nix
@@ -1,41 +1,23 @@
 {
-  description = "flake";
+  description = "not work";
 
   inputs = {
+
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    disko.url = "github:nix-community/disko/latest";
-    disko.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs-unstable.url = "github:nixpkgs/nixos-unstable"; };
+    
+    disko = {
+      url = "github:nix-community/disko/latest";
+      inputs.nixpkgs.follows = "nixpkgs"; };
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs"; };
+
     impermanence.url = "github:nix-community/impermanence";
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-  };
 
   outputs = { self, nixpkgs, disko, impermanence, home-manager, ... } @inputs: {
     nixosConfigurations = {
-      spg = nixpkgs.lib.nixosSystem {
-        system = "x86_64-Linux";
-        specialArgs = {inherit inputs;};
-        modules = [
-          disko.nixosModules.disko
-          impermanence.nixosModules.impermanence
-          #./hosts/spg/disk-config.nix
-          ./hosts/spg/configuration.nix
-          ./hosts/common/persist.nix
-          
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.codman = { ... }: {
-              imports = [
-                impermanence.homeManagerModules.impermanence
-                ./home/codman/persist.nix
-                ./home/codman/home.nix
-              ];
-            };
-          }
-        ];
-      };
       vmtest = nixpkgs.lib.nixosSystem {
         system = "x86_64-Linux";
         specialArgs = { inherit inputs; };

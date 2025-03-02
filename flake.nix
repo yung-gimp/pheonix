9a577863f2cbecd9cf1fc32231e0a4d53d141075
yung-gimp <>
2025-03-02 01:53:05 -0500

    hosts instead of hostNames because hostnames should be defined system configs

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    
+
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
--- hosts/default.nix
+++ hosts/default.nix
@@ -1,7 +1,7 @@
 { inputs, ... }: let
 
-  hostNames = builtins.attrNames (inputs.nixpkgs.lib.attrsets.filterAttrs (name: type: type == "directory") (builtins.readDir ./.));
+  hosts = builtins.attrNames (inputs.nixpkgs.lib.attrsets.filterAttrs (name: type: type == "directory") (builtins.readDir ./.));
 
-in {
-  imports = builtins.map (host: ./${host}) hostNames;
+in {  ## Eventually a function returning flake
+  imports = builtins.map (host: ./${host}) hosts;
 }

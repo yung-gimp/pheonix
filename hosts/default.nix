7e967a7e138083d14e85a999cb17c38dc988d559
yung-gimp <>
2025-03-05 23:24:00 -0500

    make imports = paths instead of strings

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,7 +1,7 @@
-{ inputs, ... }: let
+{ inputs, lib, ... }: let
 
   hosts = builtins.attrNames (inputs.nixpkgs.lib.attrsets.filterAttrs (name: type: type == "directory") (builtins.readDir ./.));
 
 in {  ## Eventually a function returning flake
-  imports = builtins.map (host: ./${host}) hosts;
+imports = builtins.map (host: ./. + /host) hosts;
 }

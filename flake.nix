902ece43150cf855f8ed775f34f4fa3f3d2a5cd0
yung-gimp <>
2025-03-07 19:04:41 -0500

    structure

--- flake.nix
+++ flake.nix
@@ -1,59 +1,44 @@
 {
-  description = "my flake";
+  description = "big gimpin";
 
   nixConfig = {
+
     substituters = [
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
     ];
 
     trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
+
   };
 
   outputs =
     inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
+
       systems = [
         "x86_64-linux"
+        "aarch64-linux"
       ];
 
       imports = [
         inputs.fpFmt.flakeModule
-        ./hosts
-        ./homes
+        ./outputs
       ];
+
     };
 
   inputs = {
-
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    disko = {
-      url = "github:nix-community/disko/latest";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    ff = {
-      url = "github:freedpom/FreedpomFlake";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows = "home-manager";
-    };
 
     fpFmt = {
       url = "github:freedpom/FreedpomFormatter";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-
-    impermanence.url = "github:nix-community/impermanence";
   };
 }

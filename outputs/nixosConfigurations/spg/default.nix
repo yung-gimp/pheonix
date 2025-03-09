dd78035cad42ebaf80727df3a31b178abb141ec0
yung-gimp <>
2025-03-08 21:02:41 -0500

    small penix

---
+++ outputs/nixosConfigurations/spg/default.nix
@@ -0,0 +1,20 @@
+{ inputs, lib, ... }:
+{
+
+  ff = {
+    system = {
+      systemd-boot.enable = true;
+    };
+    common.enable = true;
+  };
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  system.stateVersion = "24.11";
+
+  imports = [
+    inputs.ff.nixosModules.freedpomFlake
+    inputs.disko.nixosModules.disko
+    ./disko.nix
+    ./hardware.nix
+  ];
+}
---
+++ outputs/nixosConfigurations/spg/hardware.nix
@@ -0,0 +1,6 @@
+{ pkgs, ... }:
+{
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" ];
+  boot.kernelModules = [ "kvm-intel" ];
+}

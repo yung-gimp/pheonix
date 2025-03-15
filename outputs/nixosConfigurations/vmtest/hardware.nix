70470a42bef5e2bd3c923c97437e06a04be4e2ca
yung-gimp <>
2025-03-14 20:41:41 -0400

    format

--- outputs/nixosConfigurations/vmtest/hardware.nix
+++ outputs/nixosConfigurations/vmtest/hardware.nix
@@ -1,15 +1,17 @@
 { pkgs, ... }:
 {
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.initrd.availableKernelModules = [
-    "ahci"
-    "virtio_pci"
-    "xhci_pci"
-    "virtio_scsi"
-    "sd_mod"
-    "sr_mod"
-    "usbhid"
-    "hid_generic"
-  ];
-  boot.kernelModules = [ "kvm-amd" ];
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+    initrd.availableKernelModules = [
+      "ahci"
+      "virtio_pci"
+      "xhci_pci"
+      "virtio_scsi"
+      "sd_mod"
+      "sr_mod"
+      "usbhid"
+      "hid_generic"
+    ];
+    kernelModules = [ "kvm-amd" ];
+  };
 }

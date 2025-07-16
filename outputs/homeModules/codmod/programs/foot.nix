a7314d05cb673d2ec19df84ffa3033c970d26ab2
yung-gimp <172232649+yung-gimp@users.noreply.github.com>
2025-07-16 13:08:08 -0400

    feet

--- outputs/homeModules/codmod/programs/foot.nix
+++ outputs/homeModules/codmod/programs/foot.nix
@@ -1,3 +1,13 @@
-_:
+{ config, lib, ... }:
+let
+  cfg = config.cm.programs.foot
+in 
 {
+  options.cm.programs.foot.enable = lib.mkEnableOption "Enable foot terminal";
+
+  config.programs.foot = lib.mkIf cfg.enable {
+    enable = true;
+    server.enable = true;
+    settings = {};
+  };
 }

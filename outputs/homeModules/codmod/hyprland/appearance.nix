359c1d9c816a2c95b89825efa23ddd74179a901a
yung-gimp <172232649+yung-gimp@users.noreply.github.com>
2025-07-20 14:54:27 -0400

    animations

--- outputs/homeModules/codmod/hyprland/appearance.nix
+++ outputs/homeModules/codmod/hyprland/appearance.nix
@@ -12,5 +12,20 @@ in
     decoration = {
       rounding = 4;
     };
+
+    misc = {
+      disable_hyprland_logo = true;
+      disable_splash_rendering = true;
+      force_default_wallpaper = 0;
+    };
+
+    animation = [
+      "workspaces, 1, 2, workspace, slidevert"
+      "windows, 1, 2, default, gnomed"
+    ];
+
+    bezier = [
+      "workspace, 0.76, 0, 0.24, 1"
+    ];
   };
 }
